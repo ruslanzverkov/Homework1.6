d071cad7711e6845aaefffb807991a541e14bcc9
ruslanzverkov <createmed@mail.ru>
2022-07-28 23:09:22 +0300

    DZ4

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,37 @@ public class Main {
         }
         System.out.println(moneySend1);
 
+//        Второе задание
+        Arrays.sort(arr);
+        System.out.println("Максимальная сумма трат за день составила"+arr[arr.length-1]+" рублей");
+        System.out.println("Минимальная сумма трат за день составила"+arr[0]+"рублей");
+
+        int moneyMax = 0;
+        int moneyMin=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>moneyMax) {
+                moneyMax = arr[i];
+            } else if (arr[i] < moneyMin) {
+                moneyMin = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила" + moneyMax + "рублей");
+        System.out.println("Минимальная сумма трат за день составила" + moneyMin + "рублей");
+
+//        Третье задание
+
+
+        int averageSalary=0;    //=moneySend/arr.length;// смотри 1е задание
+        int moneySend3=0;
+        for (int i = 0; i < arr.length; i++) {
+            moneySend3+=arr[i];
+            averageSalary=moneySend3/arr.length;
+        }
+        System.out.println(averageSalary);
+
+//        Задание 4
+
+
     }
 
 }
