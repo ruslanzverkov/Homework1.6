54dab2c22010ba6d12bee60884ddf7d1ea876b07
ruslanzverkov <createmed@mail.ru>
2022-07-28 23:29:07 +0300

    DZFull

--- src/Main.java
+++ src/Main.java
@@ -61,6 +61,11 @@ public class Main {
 
 //        Задание 4
 
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
 
     }
 
